9a0759072b1b05a3f66404f57733beb878fbd85c
Zheko Stanchev <zheko.stanchev@gmail.com>
2024-02-06 13:49:38 +0200

    Fix: Deleting unnecessary code from Navbar

--- online-shop/src/Components/Navbar/Navbar.jsx
+++ online-shop/src/Components/Navbar/Navbar.jsx
@@ -105,48 +105,6 @@ function Navbar(props) {
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
-  // const Search = styled('div')(({ theme }) => ({
-  //   position: 'relative',
-  //   borderRadius: theme.shape.borderRadius,
-  //   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  //   '&:hover': {
-  //     backgroundColor: alpha(theme.palette.common.white, 0.25),
-  //   },
-  //   marginLeft: 0,
-  //   width: '100%',
-  //   [theme.breakpoints.up('sm')]: {
-  //     marginLeft: theme.spacing(1),
-  //     width: 'auto',
-  //   },
-  // }));
-
-  // const SearchIconWrapper = styled('div')(({ theme }) => ({
-  //   padding: theme.spacing(0, 2),
-  //   height: '100%',
-  //   position: 'absolute',
-  //   pointerEvents: 'none',
-  //   display: 'flex',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // }));
-
-  // const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  //   color: 'inherit',
-  //   width: '100%',
-  //   '& .MuiInputBase-input': {
-  //     padding: theme.spacing(1, 1, 1, 0),
-  //     // vertical padding + font size from searchIcon
-  //     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  //     transition: theme.transitions.create('width'),
-  //     [theme.breakpoints.up('sm')]: {
-  //       width: '12ch',
-  //       '&:focus': {
-  //         width: '20ch',
-  //       },
-  //     },
-  //   },
-  // }));
-
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -183,22 +141,6 @@ function Navbar(props) {
           >
             <LocalMallIcon />
           </IconButton>
-          {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={handleSearch}
-              value={queryVal}
-              onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                  handleSubmit(event);
-                }
-              }}
-            />
-          </Search> */}
           <InputBase
             sx={{ color: '#CD8E33' }}
             placeholder="Search..."
