27e149ec3b1bf1c6ec5805bdb8f27068b0f1f01a
Zheko Stanchev <zheko.stanchev@gmail.com>
2024-02-09 08:38:53 +0200

    Feat: Added FAB to AdditionalInfo page

--- online-shop/src/Components/AdditionalInfo/AdditionalInfo.jsx
+++ online-shop/src/Components/AdditionalInfo/AdditionalInfo.jsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  Grid,
+  ThemeProvider,
+  Typography,
+  alpha,
+  createTheme,
+  getContrastRatio,
+} from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getProductById } from '../../Services/products.services';
+import Fab from '@mui/material/Fab';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
+const bnmBase = '#023620';
+const bnmMain = alpha(bnmBase, 0.7);
+
+const theme = createTheme({
+  palette: {
+    bnm: {
+      main: bnmMain,
+      light: alpha(bnmBase, 0.5),
+      dark: alpha(bnmBase, 2),
+      contrastText: getContrastRatio(bnmMain, '#fff') > 4.5 ? '#fff' : '#111',
+    },
+  },
+});
 
 const AdditionalInfo = () => {
   const params = useParams();
   const [product, setProduct] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -27,80 +52,90 @@ const AdditionalInfo = () => {
   }, [params.itemId]);
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <img
-          src={product[0]?.info?.image}
-          alt="Sample"
-          style={{
-            width: '100%',
-            height: 'auto',
-            maxWidth: '100%',
-            maxHeight: '500px',
-            objectFit: 'cover',
-          }}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Typography variant="h5" sx={{ paddingLeft: 2 }}>
-          Полезна информация за продукта
-        </Typography>
-        <Typography sx={{ padding: 2 }}>
-          {product[0]?.info?.description}
-        </Typography>
-      </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <img
+            src={product[0]?.info?.image}
+            alt="Sample"
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxWidth: '100%',
+              maxHeight: '500px',
+              objectFit: 'cover',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" sx={{ paddingLeft: 2 }}>
+            Полезна информация за продукта
+          </Typography>
+          <Typography sx={{ padding: 2 }}>
+            {product[0]?.info?.description}
+          </Typography>
+        </Grid>
 
-      <Grid item xs={12} md={6}>
-        <Typography variant="h5" sx={{ paddingLeft: 2 }}>
-          Откъде идва месото
-        </Typography>
-        <Typography sx={{ padding: 2 }}>{product[0]?.info?.cut}</Typography>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" sx={{ paddingLeft: 2 }}>
+            Откъде идва месото
+          </Typography>
+          <Typography sx={{ padding: 2 }}>{product[0]?.info?.cut}</Typography>
+          <TableContainer>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell> 100g serving</TableCell>
+                  <TableCell align="right">Calories</TableCell>
+                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell component="th" scope="row">
+                    {product[0]?.title}
+                  </TableCell>
+                  <TableCell align="right">
+                    {product[0]?.info?.calories}
+                  </TableCell>
+                  <TableCell align="right">{product[0]?.info?.fats}</TableCell>
+                  <TableCell align="right">{product[0]?.info?.carbs}</TableCell>
+                  <TableCell align="right">
+                    {product[0]?.info?.proteins}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Box sx={{ position: 'fixed', bottom: 16, left: 16 }}>
+            <Fab
+              variant="extended"
+              size="small"
+              color="bnm"
+              onClick={() => navigate(-1)}
+            >
+              <NavigationIcon sx={{ mr: 1 }} />
+              Back to products
+            </Fab>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img
+            src={product[0]?.info?.diagram}
+            alt="Sample"
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxWidth: '100%',
+              maxHeight: '500px',
+              objectFit: 'cover',
+            }}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <img
-          src={product[0]?.info?.diagram}
-          alt="Sample"
-          style={{
-            width: '100%',
-            height: 'auto',
-            maxWidth: '100%',
-            maxHeight: '500px',
-            objectFit: 'cover',
-          }}
-        />
-      </Grid>
-
-      <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell> 100g serving</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell component="th" scope="row">
-                  {product[0]?.title}
-                </TableCell>
-                <TableCell align="right">
-                  {product[0]?.info?.calories}
-                </TableCell>
-                <TableCell align="right">{product[0]?.info?.fats}</TableCell>
-                <TableCell align="right">{product[0]?.info?.carbs}</TableCell>
-                <TableCell align="right">
-                  {product[0]?.info?.proteins}
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 };
 
